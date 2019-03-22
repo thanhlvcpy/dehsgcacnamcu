ceaed6bde75dd97ae170254a68b2ba7b213bb63d
thanhlvcpy <42794074+thanhlvcpy@users.noreply.github.com>
2019-03-22 10:11:04 +0700

    Add files via upload
    đề quay lui cô hòa gửi

---
+++ hoanvi.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+char t[100000];
+void print1(int n)
+{
+    for(int i=1;i<=n;i++)
+        cout<<t[i]<<" ";
+    cout<<endl;
+}
+void backtrack(int k,int n)
+{
+
+    for(int i='A';i<='B';i++)
+    {
+        t[k]=char(i);
+        if(t[k]=='B'&&t[k-1]=='B')
+            continue;
+        if(k==n)
+            print1(n);
+        else
+            backtrack(k+1,n);
+    }
+
+}
+int main()
+{
+   // freopen("hoanvi.inp","r",stdin);
+   // freopen("hoanvi.out","w",stdout);
+    int n;
+    cin>>n;
+    backtrack(1,n);
+    return 0;
+}
